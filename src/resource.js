50fc2b1d20bcb81479e9300cfe99b7e87a8b5d63
mapfap <mapkabb@gmail.com>
2014-02-26 23:22:53 +0700

    edit g_resource

--- src/resource.js
+++ src/resource.js
@@ -1,7 +1,5 @@
-var img_dot = 'images/dot.png';
+// var img_dot = 'images/dot.png';
 var g_resources = [
     //image
-    {src: img_dot },
-    {src: 'images/pillar-top.png' },
-    {src: 'images/pillar-bottom.png' }
+    // {src: img_dot },
 ];
